796b050cc34196ca3b7018de686c82f74a0464f2
DrTheteSigma <halsailani@usf.edu>
2022-10-09 17:44:39 -0400

    Created login/signup and homepage

---
+++ soccery/src/components/Mainpage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Mainpage = () => {
+  return (
+    <>
+    <div className='Home'>
+      <div className='Home-left'>
+        <button className='HomeBtn'>Join Exisiting Matches</button>
+        
+      </div>
+
+      <div className='Home-right'>
+        <button className="HomeBtn">Create New Match</button>
+      </div>
+
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Mainpage
