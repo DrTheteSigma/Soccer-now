f618ada724fd4e9d735090727dc96186cb25d2b5
DrTheteSigma <halsailani@usf.edu>
2022-10-09 20:01:07 -0400

    added submit button to createGame

--- Frontend/src/components/CreatingGame.js
+++ Frontend/src/components/CreatingGame.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+
 
 export default function CreatingGame() {
     const [date, setDate] = useState('')
     const [location, setLocation] = useState('')
     const [description, setDescription] = useState('')
 
+    const navigate = useNavigate();
+
+    const createMatch = async () => {  
+      await axios.post("http://localhost:8080/v1/matches/create", {
+          date, location
+      })
+      navigate("/", {replace: true});
+  }
+
 
   return (
+
     <div className="create-game-background">
       <div>
           <h1 className='create-game-title'>Create Game</h1>
@@ -20,6 +33,8 @@ export default function CreatingGame() {
                 <br/>
                 <label>Description</label> 
                 <textarea onClick={(e) => {setDescription(e.target.value)}}></textarea>
+                <br></br>
+                <button onClick={createMatch} type='button'>Submit</button>
             </form>
           </div>
           
